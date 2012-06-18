eb2d29e70aa0c9ecf7ccd8f745c712b42afdc37b
aur1mas <aur1mas@devnet.lt>
2012-06-18 10:13:57 +0300

    removed old files & added controller test cases

---
+++ ZendA/Test/PHPUnit/ControllerTestCase.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package ZendA
+ * @author aur1mas <aur1mas@devnet.lt>
+ */
+class ZendA_Test_PHPUnit_ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase
+{
+
+	protected $application;
+
+	protected function setUp()
+	{    
+		$this->bootstrap = array($this, 'appBootstrap');
+		parent::setUp();
+	}
+
+	public function appBootstrap() {
+		$this->application = new Zend_Application(
+			APPLICATION_ENV,
+			APPLICATION_PATH . '/configs/application.ini'
+		);
+
+		$this->_frontController->addModuleDirectory(APPLICATION_PATH . "/modules");
+		$this->_frontController->setDefaultControllerName('index')
+		    ->setDefaultAction('index')
+		    ->setDefaultModule('default');
+		
+		Zend_Controller_Action_HelperBroker::addHelper(new Zend_Layout_Controller_Action_Helper_Layout);
+	}
+}
